5710a3a763faf54b3173d01862bb54afede5fb13
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2021-07-24 11:02:40 -0500

    Create helper.h

---
+++ lab8/helper.h
@@ -0,0 +1,23 @@
+//Programmer: Midori Lynch             Username: mmlgqz
+//Instructor name: Saiteja Konda       Section: CS 1580 Section B
+//Date: 10/08/2019                     File: helper.cpp
+//Purpose: This file contains the function definitions for the
+//         functions used in the area of a  calculator program
+
+#include "helper.h"
+
+#include <iostream>
+
+using namespace std;
+
+void greet()
+{
+  cout<<"****Welcome user****"<<endl;
+  return;
+}
+
+void signout()
+{
+  cout<<"****Thanks for using the circle area calculator!!!********"<<endl;
+  return;
+}
